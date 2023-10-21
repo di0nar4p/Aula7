d9a9f6f81f593d6064b18e530d383ed7db21b7f0
Albert Dias <albrtcd@gmail.com>
2023-10-21 12:26:50 -0300

    Modificação do código proposto

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import os
-def soma(num1:int,num2:int) -> int:
-    return num1+num2
+def somar(num1:int, num2:int) -> int:
+    return num1 + num2
+
+def subtrair(num1:int,num2:int) -> int:
+    return num1 - num2
+
+def multiplicar(num1:int, num2:int) -> int:
+    return num1 * num2
+
+def dividir(num1:int, num2:int) -> int:
+    if num2 == 0:
+        return "OPERAÇÃO INVÁLIDA!"
+    else:
+        return num1 / num2
 
 while True:
     print('''
@@ -23,13 +35,49 @@ while True:
 
 
         case 1:
-            num1= int(input('Digite o primeiro valor: '))
-            num2= int(input('Digite o segundo valor: '))
-            total= soma(num1,num2)
+            num1 = int(input('Digite o primeiro valor: '))
+            num2 = int(input('Digite o segundo valor: '))
+            total = somar(num1, num2)
 
-            print(f'\nA soma de {num1} + {num2}= {total}')
-            loop= input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            print(f'\nA soma de {num1} + {num2} = {total}')
+            loop = input('\nDeseja fazer outra operação? (S/N)  ').upper()
             if loop == 'N':
                 break
             else:
                 os.system('cls') 
+
+        case 2:
+            num1 = int(input('Digite o primeiro valor: '))
+            num2 = int(input('Digite o segundo valor: '))
+            total = subtrair(num1, num2)
+
+            print(f'\nA subtração de {num1} - {num2} = {total}')
+            loop = input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls') 
+
+        case 3:
+            num1 = int(input('Digite o primeiro valor: '))
+            num2 = int(input('Digite o segundo valor: '))
+            total = multiplicar(num1, num2)
+
+            print(f'\nA soma de {num1} * {num2} = {total}')
+            loop = input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls') 
+
+        case 4:
+            num1 = int(input('Digite o primeiro valor: '))
+            num2 = int(input('Digite o segundo valor: '))
+            total = dividir(num1, num2)
+
+            print(f'\nA soma de {num1} / {num2} = {total}')
+            loop = input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls')
