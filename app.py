3cea3783bfdacf78a0abcd5ad1d87080c5a7ba07
JEAN LUC <145208888+jeanlucdev@users.noreply.github.com>
2023-10-21 12:31:47 -0300

    Funções criadas

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import os
 def soma(num1:int,num2:int) -> int:
     return num1+num2
 
+def subtracao(num1:int,num2:int) -> int:
+    return num1-num2
+
+def multiplicacao(num1:int,num2:int) -> int:
+    return num1*num2
+
+def divisao(num1:int,num2:int) -> int:
+    return num1/num2
+
 while True:
     print('''
                                     =========== CALCULADORA ===========
@@ -33,3 +42,46 @@ while True:
                 break
             else:
                 os.system('cls') 
+
+        
+        case 2:
+            num1= int(input('Digite o primeiro valor: '))
+            num2= int(input('Digite o segundo valor: '))
+            total= subtracao(num1,num2)
+
+            print(f'\nA soma de {num1} - {num2}= {total}')
+            loop= input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls') 
+
+        
+        case 3:
+            num1= int(input('Digite o primeiro valor: '))
+            num2= int(input('Digite o segundo valor: '))
+            total= multiplicacao(num1,num2)
+
+            print(f'\nA soma de {num1} x {num2}= {total}')
+            loop= input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls') 
+
+        
+        case 4:
+            num1= int(input('Digite o primeiro valor: '))
+            num2= int(input('Digite o segundo valor: '))
+            total= divisao(num1,num2)
+
+            print(f'\nA soma de {num1} / {num2}= {total}')
+            loop= input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls') 
+
+        case 0:
+            print('Sair do programa!')
+            break
