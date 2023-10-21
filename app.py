e3329e69e023e0628c955a00410ae1d1cc68460a
Aluno da Infinity School <aluno@3D.local>
2023-10-21 12:23:31 -0300

    feat: Adicionei as funcoes de subtração, divisão, e multiplicação

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import os
 def soma(num1:int,num2:int) -> int:
     return num1+num2
 
+def subtracao(num1:int,num2:int) -> int:
+    return num1-num2
+
+def multiplicacao(num1:int,num2:int) -> int:
+    return num1*num2
+
+def divisao(num1:int,num2:int) -> int:
+    try:
+        if num2 == 0:
+            raise ZeroDivisionError
+    except ZeroDivisionError:
+        return 'Não é possivel dividir um valor por zero!'
+    return num1 / num2
+
 while True:
     print('''
                                     =========== CALCULADORA ===========
@@ -15,19 +29,52 @@ while True:
           0- SAIR
           ''')
 
-
     op= int(input('\nDigite o número da operação que deseja: '))
 
-
     match op:
-
 
         case 1:
             num1= int(input('Digite o primeiro valor: '))
             num2= int(input('Digite o segundo valor: '))
             total= soma(num1,num2)
 
-            print(f'\nA soma de {num1} + {num2}= {total}')
+            print(f'\nA soma de {num1} + {num2} = {total}')
+            loop= input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls') 
+
+        case 2:
+            num1= int(input('Digite o primeiro valor: '))
+            num2= int(input('Digite o segundo valor: '))
+            total= subtracao(num1,num2)
+
+            print(f'\nA subtração de {num1} - {num2} = {total}')
+            loop= input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls') 
+
+        case 3:
+            num1= int(input('Digite o primeiro valor: '))
+            num2= int(input('Digite o segundo valor: '))
+            total= multiplicacao(num1,num2)
+
+            print(f'\nA multiplicação de {num1} * {num2} = {total}')
+            loop= input('\nDeseja fazer outra operação? (S/N)  ').upper()
+            if loop == 'N':
+                break
+            else:
+                os.system('cls') 
+
+        case 4:
+            num1= int(input('Digite o primeiro valor: '))
+            num2= int(input('Digite o segundo valor: '))
+            total= divisao(num1,num2)
+
+            print(f'\nA divisão de {num1} / {num2} = {total}')
             loop= input('\nDeseja fazer outra operação? (S/N)  ').upper()
             if loop == 'N':
                 break
